caebd929de74ac68852871f1b4269cf56152e3dc
zhangn <zhang.ning@mdb-fintech.com>
2019-12-11 18:13:19 +0800

    文档初始化

---
+++ docs/.vuepress/components/hello.vue
@@ -0,0 +1,11 @@
+<template>
+  <div style="color:red;">Hello World</div>
+</template>
+<script>
+export default {
+  name: "hello",
+  data(){
+    return{}
+  }
+}
+</script>
---
+++ docs/README.md
@@ -0,0 +1,7 @@
+# vue-dy-form 使用教程
+
+
+# <span v-for="i in 3">{{i}}</span>
+
+
+<hello></hello>
